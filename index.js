9031a667d48abdf4b752beb243009491783e3156
Max Malysh <someone307@gmail.com>
2015-09-28 21:49:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -27,4 +27,26 @@ app.get('/', function(req, res) {
     });
 })
 
+var lastResult = "";
+
+app.get('/background', function(req, res) {
+    var child = childProcess.execFile(binPath, childArgs, function(err, stdout, stderr) {
+        console.log('stdout ', stdout);
+        console.log('stderr ', stderr);
+        console.log('err', err);
+        lastResult = "";
+        res.send("Processing in background now");
+    })
+
+    // use event hooks to provide a callback to execute when data are available: 
+    child.stdout.on('data', function(data) {
+        lastResult += data.toString();
+        console.log(data.toString());
+    });
+})
+
+app.get('/last', function(req, res) {
+    res.send(lastResult);
+})
+
 app.listen(process.env.PORT || 3000)
