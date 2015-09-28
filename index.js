3b1d3e50166396cd1e1c6df442e5a73fd0d1d514
Max Malysh <someone307@gmail.com>
2015-09-28 20:14:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.get('/', function(req, res) {
         console.log('stdout ', stdout);
         console.log('stderr ', stderr);
         console.log('err', err);
-        res.send(result);
+        res.write(result);
     })
 
     // use event hooks to provide a callback to execute when data are available: 
